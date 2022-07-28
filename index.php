3f603dbdcfff26ce017244bbad15c3833a19bb97
P! <denixmas123@gmail.com>
2022-07-28 16:16:28 -0600

    PrimerSitioWeb

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenida</title>
+</head>
+
+<body>
+    <header>
+        <h1>MI HOJA DE VIDA</h1>
+    </header>
+    <div id="menu">
+        <nav>
+            <ul>
+                <li class="sel"><a href="#">Bienvenida</a></li>
+                <li><a href="datosPersonales.html">Datos Personales</a></li>
+                <li><a href="experienciaLaboral.html">Experiencia laboral</a></li>
+            </ul>
+
+        </nav>
+    </div>
+
+    <div class="img">
+        <figure >
+            <img src="/src/curriculum.png" alt="img curriculum" class="dimensionImg">            
+        </figure>
+    </div>
+
+
+    <footer>
+        <p>Todos los derechos reservados a: Denilson Ramírez</p>
+    </footer>
+    <link rel="stylesheet" href="styles.css">
+</body>
+
+</html>
